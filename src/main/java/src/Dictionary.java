aa161ad81fb142c1ff3f45317ece383b0c573436
christian.passold <passold.christian@hotmail.com>
2018-03-11 16:09:18 -0300

    Geração do dicionário

--- src/main/java/src/Dictionary.java
+++ src/main/java/src/Dictionary.java
@@ -2,39 +2,76 @@ package src;
 
 import src.exception.FileLineLimitException;
 import src.exception.LineLengthException;
+import src.util.IOUtils;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Dictionary {
 
     public static final int LINE_SIZE_LIMIT = 200;
     public static final int FILE_LINE_LIMIT = 10000;
+    public static final int DICTIONARY_WORDS_LIMIT = 5000;
 
-    private Map<Integer, String> mapLines;
+    private Map<Integer, String> lineStringMap;
+    private TreeSet<String> words;
 
     private Dictionary(Map<Integer, String> lines) {
-        mapLines = lines;
+        lineStringMap = lines;
+        words = new TreeSet<String>();
+
+        process();
     }
 
+    private void addToDictionary(String word) {
+        if (word != null && !word.isEmpty()) {
+            if (!words.contains(word.toLowerCase())) {
+                words.add(word.toLowerCase());
+            }
+        }
+    }
 
+    private void process() {
 
+        Set<Map.Entry<Integer, String>> entries = lineStringMap.entrySet();
 
+        for (Map.Entry<Integer, String> line : entries) {
 
+            if (words.size() > DICTIONARY_WORDS_LIMIT) {
+                break;
+            }
 
+            String value = line.getValue();
+            if (value == null) {
+                continue;
+            }
 
+            if (value.isEmpty()) {
+                continue;
+            }
 
+            String[] lineWords = value.split("[^a-zA-Z]+");
 
+            for (String lineWord : lineWords) {
+                addToDictionary(lineWord);
+            }
 
+        }
+    }
 
+    public void show() {
 
+        IOUtils.writeConsole("\n\n\n\n");
+        IOUtils.writeConsole("------------- INICIO DICIONARIO -------------------");
 
+        for (String word : this.words) {
+            IOUtils.writeConsole(word);
+        }
 
-
-
-
+        IOUtils.writeConsole("------------- FIM DICIONARIO -------------------");
+        IOUtils.writeConsole("\n\n\n\n");
+    }
 
 
     static class Builder {
--- src/main/java/src/util/IOUtils.java
+++ src/main/java/src/util/IOUtils.java
@@ -4,6 +4,9 @@ import java.io.*;
 
 public class IOUtils {
 
+    private static final String EXIT_APPLICATION = "exit";
+
+
     public static BufferedReader loadFile(String path) throws IOException {
 
         File file = new File(path);
@@ -18,9 +21,44 @@ public class IOUtils {
         return new BufferedReader(new FileReader(path));
     }
 
-    public static String readFromConsole() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        return br.readLine();
+    public static BufferedReader readFileFromConsole() throws IOException {
+        boolean exit = false;
+
+        writeConsole(String.format("Para sair informe a palavra chave '%s'", EXIT_APPLICATION));
+        writeConsole("Informe o caminho do arquivo a ser computado:");
+        String path;
+        BufferedReader fileBufferedReader = null;
+
+        do {
+            try {
+                path = new BufferedReader(new InputStreamReader(System.in)).readLine();
+
+                if (EXIT_APPLICATION.equalsIgnoreCase(path)) {
+                    System.exit(0);
+                }
+
+                if (path != null && !path.isEmpty()) {
+                    fileBufferedReader = IOUtils.loadFile(path);
+                    exit = true;
+                }
+
+
+            } catch (IOException e) {
+                logError(e);
+            }
+
+        } while (!exit);
+
+        return fileBufferedReader;
+    }
+
+
+    public static void logError(Throwable e) {
+        System.out.println("Ocorreu um problema, tente novamente: " + e.getMessage());
+    }
+
+    public static void writeConsole(String s) {
+        System.out.println(s);
     }
 
 }
